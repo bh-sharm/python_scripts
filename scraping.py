7f7e4cffb20632db01a06310261740d86343d2ae
Bharat <bharatsharma61@hotmail.com>
2017-05-11 09:26:58 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,5 +1,15 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-open_url = urlopen("https://ca.indeed.com/jobs?q=research+analyst&l=Ontario")
-soup = BeautifulSoup(open_url.read(), "lxml")
+try:
+    open_url = urlopen("https://ca.indeed.com/jobs?q=research+analyst&l=Ontario")
+    soup = BeautifulSoup(open_url.read(), "lxml")
+except HTTPError as he:
+    print(he)
+    exit()
+except AttributeError as ae:
+    print(ae)
+    exit()
+
 print(soup.title)
+
