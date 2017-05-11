37ceb9d34fd7dbeb5e50831084e01b8f1401a867
Bharat <bharatsharma61@hotmail.com>
2017-05-11 10:16:06 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -11,5 +11,22 @@ except AttributeError as ae:
     print(ae)
     exit()
 
+# Choose whaterver you are interested into
 print(soup.title)
 
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
+print(soup.title.text)
+print(soup.p)
+
+print(soup.find_all('p')) # good for news websites
+
+for paragraph in soup.find_all('p'):
+    print(paragraph.string) # if no child tag:
+    print(paragraph.text) # most cases
+
+print(soup.get_text())
+
+for url in soup.find_all('a'):
+    print(url.get('href'))
